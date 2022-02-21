d22561c47029c200fd628814f6ef2086317bfdb6
anangyoga <aharwinto@gmail.com>
2022-02-21 20:17:09 +0700

    docs: asset management --loading css, images, fonts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from "lodash";
+import "./style.css";
+import Squidward from "./squidward.png";
 
 function component() {
   const element = document.createElement("div");
 
   //   Lodash, now imported by this script
   element.innerHTML = _.join(["Itsar", "Bentala", "Akalendra"], " ");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Squidward;
+
+  element.appendChild(myIcon);
   return element;
 }
 
